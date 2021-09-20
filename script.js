c21deda8d507588f221b83885ef25f4b9196da77
Marcos Martinelli <marcos.joaquim.martinelli@gmail.com>
2021-09-20 13:43:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,74 +3,59 @@ const off10 = document.querySelector('.off10')
 const off15 = document.querySelector('.off15')
 const off20 = document.querySelector('.off20')
 const modal = document.querySelector('.modal')
+const roleta = document.querySelector('.roleta')
+const pointer = document.querySelector('.pointer')
+const widthScreen = document.querySelector('body').offsetWidth
+let elCoordenadas = pointer.getBoundingClientRect();
 let valueClick = ""
-
-off5.addEventListener('click', () => {
-    modal.classList.add('active')
-    document.querySelector('.modal h1').innerHTML = "5% DE DESCONTO EM TODOS NOSSOS PRODUTOS!"
-    valueClick = off5.value
-})
-off10.addEventListener('click', () => {
-    modal.classList.add('active')
-    document.querySelector('.modal h1').innerHTML = "10% DE DESCONTO EM TODOS NOSSOS PRODUTOS!"
-    valueClick = off10.value
-})
-off15.addEventListener('click', () => {
-    modal.classList.add('active')
-    document.querySelector('.modal h1').innerHTML = "15% DE DESCONTO EM TODOS NOSSOS PRODUTOS!"
-    valueClick = off15.value
-})
-off20.addEventListener('click', () => {
-    modal.classList.add('active')
-    document.querySelector('.modal h1').innerHTML = "20% DE DESCONTO EM TODOS NOSSOS PRODUTOS!"
-    valueClick = off20.value
-})
-
-let roleta = 0
+let mathRoleta = 0
 let click = 0
-function playRoleta(){
-    click = click+=1
-    if(click === 1){
-        roleta = roleta += (Math.random() * 1000)*10;
-        console.log(roleta)
+
+for (let i = 1; i < 5; i++) {
+    let promo = document.querySelector(`.roleta button:nth-child(${i})`)
+    promo.addEventListener('click', () => {
+        modal.classList.add('active')
+        document.querySelector('.modal h1').innerHTML = `${promo.value} DE DESCONTO EM TODOS NOSSOS PRODUTOS!`
+        valueClick = promo.value
+    })
+}
+
+function playRoleta() {
+    click = click += 1
+    if (click === 1) {
+        mathRoleta = mathRoleta += (Math.random() * 1000) * 10;
+        console.log(mathRoleta)
+        if (mathRoleta < 1000) mathRoleta = mathRoleta + 2000;
+        console.log(mathRoleta)
         modal.classList.remove('active')
-        
-        document.querySelector('.roleta').style.transform = `rotate(${roleta}deg)`
-        
+
+        document.querySelector('.roleta').style.transform = `rotate(${mathRoleta}deg)`
+
         setTimeout(() => {
             document.querySelector('.roleta').classList.add('activeClick')
             document.elementFromPoint((elCoordenadas.x + 49.9), (elCoordenadas.y + 200)).click();
             document.querySelector('.roleta').classList.remove('activeClick')
 
-            if(valueClick === ""){
+            if (valueClick === "") {
                 click = 0
                 alert('Por favor, gire a roleta novamente')
             }
         }, 5500)
-        
-    }else{
+
+    } else {
         alert('Você já pegou o seu desconto!')
     }
 }
-
-let el = document.getElementById('roleta');
-let elCoordenadas = el.getBoundingClientRect();
 
 function closeModal() {
     modal.classList.remove('active')
 }
 
-let widthScreen = document.querySelector('body').offsetWidth
-
-
-function responsiveScreen () {
-    let roleta = document.querySelector('.roleta')
-    let pointer = document.querySelector('.pointer')
-    let modal = document.querySelector('.modal')
+function responsiveScreen() {
     roleta.style.width = '300px'
     roleta.style.height = '300px'
     pointer.style.paddingBottom = '470px'
     modal.style.width = '80vw'
 }
 
-if(widthScreen < 450) responsiveScreen()
+if (widthScreen < 450) responsiveScreen()
